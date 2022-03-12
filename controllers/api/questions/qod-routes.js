f3e67d92f6f0764c47c409335545ac87b801e548
MI-Optimist <ajureelmi@gmail.com>
2022-03-12 11:44:05 -0500

    added comments

--- controllers/api/questions/qod-routes.js
+++ controllers/api/questions/qod-routes.js
@@ -1,41 +1,32 @@
 const router = require("express").Router();
 const {QoD} = require('../../../models');
-const date = require('date-and-time');
-
-//import fetch from 'node-fetch';
-//  import fetch from 'node-fetch';
-//  ^^^^^^
-// SyntaxError: Cannot use import statement outside a module
-
-const fetch = require('node-fetch');
-const { res } = require("date-and-time");
-// const fetch = require('node-fetch');
-//               ^
-// Error [ERR_REQUIRE_ESM]: require() of ES Module C:\Users\james\OneDrive\Desktop\Bootcamp\Working\TriviaUS\node_modules\node-fetch\src\index.js from C:\Users\james\OneDrive\Desktop\Bootcamp\Working\TriviaUS\controllers\api\questions\qod-routes.js 
-// not supported.
-// Instead change the require of index.js in C:\Users\james\OneDrive\Desktop\Bootcamp\Working\TriviaUS\controllers\api\questions\qod-routes.js 
-// to a dynamic import() which is available in all CommonJS modules.
-
+const date = require('date-and-time'); //eables us to compare dates
+const fetch = require('node-fetch'); //enables us to fetch from 3rd party apis
+//I think we can delete this next line
+// const { res } = require("date-and-time"); 
 
 const today = new Date();
 
 // new promise will go inside of router
 // get our latest question and determine if it is from today or not
 // returns latest question or 'new' if a new question is needed
+// output is used by newQoDPromise which either passes through the question or gets a new one
 const getQoDPromise = () => {
   return new Promise(function(resolve, reject){
+    // get the latest question
     QoD.findOne({
       order: [ [ 'id', 'DESC' ]]
       })
       .then(dbqod =>{
+        // check if the question was created today
         const qdate = dbqod.createdAt;
         if (date.isSameDay(qdate, today)) {
           console.log (qdate, today, date.isSameDay(qdate,today));
-          resolve( dbqod);
+          resolve( dbqod); //question was from today, return the question
         } else {
           console.log (qdate, today, date.isSameDay(qdate,today));
           console.log ('uh-oh');
-          resolve ('new');
+          resolve ('new'); //question was not from today, return flag to get a new question
         }
       } )
   });
@@ -44,6 +35,7 @@ const getQoDPromise = () => {
 const newQoDPromise = (qod) => {
   return new Promise(function(resolve, reject){
     if (qod == 'new'){
+      // get a new question
       var requestUrl = 'https://opentdb.com/api.php?amount=1';
       fetch(requestUrl)
       .then(res => res.json())
@@ -52,19 +44,16 @@ const newQoDPromise = (qod) => {
           question: json.results[0]
           }).then(q => {
             console.log('q.q', q.question);
-            resolve (q);}))
+            resolve (q.question);}))
       .catch(err => {
         console.log(err);
         res.status(500).json(err);
       });
-    } else {resolve (qod)}
+    } else {resolve (qod.question)} //didn't need a new question
   })
 }
 
-
-
-
-
+// ************************** Get "/" Route ************************
 // EXAMPLE:
 // http://localhost:3001/api/q/qod/
 // Returns json of the most recent question
