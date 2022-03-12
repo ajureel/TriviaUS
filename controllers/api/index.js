3bd21d6887c867987ef9433556dd7164bfe2818d
MI-Optimist <ajureelmi@gmail.com>
2022-03-12 11:56:19 -0500

    updated room data fields, added room route

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,10 +1,12 @@
 const router = require("express").Router();
 //const apiRoutes = require("./api");
 const questionRoutes = require("./questions");
+const roomRoutes = require(".room-routes");
 
 console.log("api index");
 
 router.use('/q',questionRoutes);
+router.use('/room',roomRoutes);
 //router.use(require(apiRoutes));
 
 module.exports = router;
--- models/room.js
+++ models/room.js
@@ -3,7 +3,6 @@ const sequelize = require('../config/connection');
 
 // create our Post model
 class Room extends Model {}
-  
 
 // create fields/columns for Post model
 Room.init(
@@ -17,6 +16,18 @@ Room.init(
       title: {
         type: DataTypes.STRING,
         allowNull: false
+      },
+      num_q: {
+        type: DataTypes.INTEGER,
+        defaultValue: 5
+      },
+      time_q: {
+        type: DataTypes.INTEGER,
+        defaultValue: 1
+      },
+      max_players: {
+        type: DataTypes.INTEGER,
+        defaultValue: 10
       },
       room_code: {
         type: DataTypes.STRING,
