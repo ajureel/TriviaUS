e3b1bcb1dd6dda9e7789bbf48126194a270fdec8
Ricky27 <Rickyrenteria7@gmail.com>
2022-03-12 20:59:52 -0800

    start login

---
+++ controllers/login-routes.js
@@ -0,0 +1,18 @@
+//this file is for the login functions
+const express = require('express');
+const router = require("express").Router();
+
+const loginPage = () => {
+    //mock data
+}
+
+
+
+
+
+router.get('', (res, req) => {
+    //not sure what to put in here 
+})
+
+
+module.exports = router;
