acfa60cb70cdca6537e349859175fe534030c996
MI-Optimist <ajureelmi@gmail.com>
2022-03-19 10:21:06 -0400

    synched up with the front end stuff from james

---
+++ public/index.js
@@ -0,0 +1,27 @@
+// Random Variables
+var questionNum = 0;
+var correctAns = 0;
+var questionIndex = 0;
+var scoreResult;
+// End of Random Variables
+
+// Connecting Elements
+var timer = document.getElementById("timer");
+var timeLeft = document.getElementById("timeLeft");
+var timesUp = document.getElementById("timesUp");
+
+var startDiv = document.getElementById("start");
+var startQuizBtn = document.getElementById("start-quiz-button");
+
+var questionDiv = document.getElementById("questionDiv");
+var questionTitle = document.getElementById("questionTitle");
+var choiceA = document.getElementById("btn0");
+var choiceB = document.getElementById("btn1");
+var choiceC = document.getElementById("btn2");
+var choiceD = document.getElementById("btn3");
+var answerCheck = document.getElementById("answerCheck");
+
+var summary = document.getElementById("summary");
+var submitInitialBtn = document.getElementById("submitInitialBtn");
+var initialInput = document.getElementById("initialInput");
+var everything = document.getElementById("everything"); 
